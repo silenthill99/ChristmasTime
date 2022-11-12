7e4d4bfbf4369dcd0c5f53d53cba231322ae729b
silenthill99 <florian.graziani@sfr.fr>
2022-11-12 20:39:27 +0100

    Ajout des outils en sucre d'orge

--- src/main/java/fr/silenthill99/christmas_time/init/ModItems.java
+++ src/main/java/fr/silenthill99/christmas_time/init/ModItems.java
@@ -11,9 +11,15 @@ public class ModItems
 {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MODID);
 
+    //Nourriture
     public static final RegistryObject<Item> BARLEY = ITEMS.register("barley", () -> new Item(new Item.Properties().food(new Food.Builder().alwaysEat().build()).tab(ItemGroup.TAB_MATERIALS)));
 
     public static final RegistryObject<Item> BARLEY_SWORD = ITEMS.register("barley_sword", () -> new SwordItem(ModItemTiers.BARLEY, 3, -3f, new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_COMBAT)));
     public static final RegistryObject<Item> BARLEY_PICKAXE = ITEMS.register("barley_pickaxe", () -> new PickaxeItem(ModItemTiers.BARLEY, 3, -3f, new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> BARLEY_AXE = ITEMS.register("barley_axe", () -> new AxeItem(ModItemTiers.BARLEY, 1, -3f, new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> BARLEY_HOE = ITEMS.register("barley_hoe", () -> new HoeItem(ModItemTiers.BARLEY, 0, -3, new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(1)));
+    public static final RegistryObject<Item> BARLEY_SHOVEL = ITEMS.register("barley_shovel", () -> new ShovelItem(ModItemTiers.BARLEY, 1, -3f, new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(1)));
+
+    //Items de blocs
     public static final RegistryObject<Item> GINGERBREAD_SUGAR = ITEMS.register("gingerbread_sugar", () -> new BlockItem(ModBlocks.GINGERBREAD_SUGAR.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
 }
