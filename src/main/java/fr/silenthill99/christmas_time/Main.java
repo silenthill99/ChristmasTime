f1a7751c08eec4c03109dcddfa230b9414c7772c
silenthill99 <florian.graziani@sfr.fr>
2022-11-12 12:54:59 +0100

    Initial commit

---
+++ src/main/java/fr/silenthill99/christmas_time/Main.java
@@ -0,0 +1,28 @@
+package fr.silenthill99.christmas_time;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Main.MODID)
+public class Main
+{
+    public static final String MODID = "christmas_time";
+
+    public Main()
+    {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
+        bus.addListener(this::clientSetup);
+    }
+
+    private void setup(FMLCommonSetupEvent event)
+    {
+    }
+
+    private void clientSetup(FMLClientSetupEvent event)
+    {
+    }
+}
